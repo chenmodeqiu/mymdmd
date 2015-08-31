5522362a7748e537bb7cd2a88cca05a578c70133
chenmodeqiu <532823023@qq.com>
2015-08-31 15:55:35 +0800

    项目初始化

---
+++ mdmdproject/mdmdproject/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  mdmdproject
+//
+//  Created by xueshiqin on 15/8/31.
+//  Copyright (c) 2015年 xueshiqin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ mdmdproject/mdmdproject/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  mdmdproject
+//
+//  Created by xueshiqin on 15/8/31.
+//  Copyright (c) 2015年 xueshiqin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
